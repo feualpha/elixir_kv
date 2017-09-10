049ff891028d7adea4efcfbfad5d3a78f3f74a66
feualpha <feualpha@gmail.com>
2017-09-10 17:07:14 +0700

    initial commit

---
+++ lib/elixir_kv.ex
@@ -0,0 +1,18 @@
+defmodule ElixirKv do
+  @moduledoc """
+  Documentation for ElixirKv.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirKv.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
